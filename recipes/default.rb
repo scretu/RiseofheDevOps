4c29197f9a41271354f9c737c09831f8c94c33d6
Silvian Cretu <silvian.cretu@3pillarglobal.com>
2017-06-10 15:18:01 +0300

    adding files

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,3 +12,14 @@ service 'apache2' do
   supports status: true
   action [:enable, :start]
 end
+
+=begin
+include_recipe 'apt'
+
+apt_repository 'nginx' do
+       uri 'http://nginx.org/packages/debian/'
+       distribution "#{node["lsb"]["codename"]}"
+       trusted true
+       key "nginx_signing.key"
+end
+=end
